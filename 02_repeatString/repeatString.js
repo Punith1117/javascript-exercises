0e60e8b2d668ed3ee92cd30282ae03d9748de757
Punith1117 <punith11172006@gmail.com>
2024-08-09 13:06:32 +0530

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, noTimes) {
+    if ( noTimes >= 0 ) {
+        let resultWord = '';
+    for (let i = 0; i < noTimes; i++) {
+        resultWord += word;
+    }
+    return resultWord;
+    } else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
