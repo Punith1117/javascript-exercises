69a0dc7ba1a1eaf0f1f93784885cb5a1a2db5765
Punith1117 <punith11172006@gmail.com>
2024-08-09 15:44:35 +0530

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arg) {
+    let newArray = arg[0]; 
+    for (let i = 1; i < arg.length; i++) {
+        for (let j = 0; j < newArray.length; j++) {
+            if (newArray[j] === arg[i]) {
+                newArray.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
