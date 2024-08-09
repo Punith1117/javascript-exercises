04ca9227b839f906ef0ccec1a3f8dc0f8a0842a9
Punith1117 <punith11172006@gmail.com>
2024-08-09 16:19:22 +0530

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+function getSum(a, b) {
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    } 
 
+    return sum;
+}
+const sumAll = function(num1, num2) {
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0) {
+        if (num1 < num2) {
+            return getSum(num1, num2);
+        } else {
+            return getSum(num2, num1);
+        }
+    } else {
+        return 'ERROR';
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
