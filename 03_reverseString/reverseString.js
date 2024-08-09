b09e98118da5e673da8ea354e73bf3043693163f
Punith1117 <punith11172006@gmail.com>
2024-08-09 13:35:22 +0530

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let resultString = [];
+    for (let i = 0; i < inputString.length; i++) {
+        resultString.push(inputString[inputString.length - i - 1]);
+    }
+    return resultString.join('');
 };
 
 // Do not edit below this line
