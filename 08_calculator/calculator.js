d85a80c068ccaf2c1332fcf0485dd58560b774fd
Punith1117 <punith11172006@gmail.com>
2024-08-10 12:09:25 +0530

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(findSum);
+
+  function findSum(item) {
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(number => {
+    product *= number;
+  });
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let product = 1;
+  for (i = 0; i < power; i++) {
+    product *= base;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(max) {
+	let fac = 1;
+  for ( i = max; i > 0; i--) {
+    fac *= i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
