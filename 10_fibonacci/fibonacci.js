b17ae835d4d39f3a6e7c78891b4c1ce31e6e16a1
Punith1117 <punith11172006@gmail.com>
2024-08-10 13:54:32 +0530

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let int = parseInt(n);
 
+    if (int < 0) return "OOPS";
+    else if (int == 0 || int == 1) {
+        return int;
+    } else {
+        let firstNum = 0;
+        let secondNum = 1;
+        for (i = 2; i <= n; i++) {
+            fibNum = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = fibNum; 
+        }
+        return fibNum;
+    }
 };
 
 // Do not edit below this line
