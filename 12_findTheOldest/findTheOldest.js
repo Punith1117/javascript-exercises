e35720d2e87616a932fb647a4c61fa55eeef6006
Punith1117 <punith11172006@gmail.com>
2024-08-10 17:22:04 +0530

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(x) {
+    const currentYear = new Date().getFullYear();
+    const people = [];
 
+    x.forEach(person => {
+        let personAge = 0;
+        if (person.yearOfDeath == undefined) {
+            personAge = currentYear - person.yearOfBirth;
+        } else {
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        }
+        const personObject = {
+            name: person.name,
+            age: personAge
+        };
+        
+        people.push(personObject);  
+    });
+
+    const maxAge = Math.max(...people.map(person => person.age));
+    const personWithMaxAge = people.find(person => person.age === maxAge);
+
+    const nameOfMaxAge = personWithMaxAge.name;
+    let name = [];
+    name.push(nameOfMaxAge);
+    console.log(name[0]);
+    return name[0];
 };
 
 // Do not edit below this line
