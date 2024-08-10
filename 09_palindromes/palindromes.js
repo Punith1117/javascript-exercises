a19d1fa215b29018a2aa07ad5e42882326bd21f0
Punith1117 <punith11172006@gmail.com>
2024-08-10 13:18:09 +0530

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.replace(/[,!. ]/g,'');
+    let lowerCaseStr = cleanStr.toLowerCase();
+    let strArray = [];
+    reverseStrArray = lowerCaseStr.split('').reverse().join('');
+    if (lowerCaseStr == reverseStrArray) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
